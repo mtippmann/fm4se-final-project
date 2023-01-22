37627caec49b76f010919ddc5a146e692a5d34ee
Martin Tippmann <martin.tippmann@uni-weimar.de>
2023-01-22 17:18:59 +0100

    get sigs, funcs

--- src/main/java/de/buw/fm4se/Project.java
+++ src/main/java/de/buw/fm4se/Project.java
@@ -2,7 +2,9 @@ package de.buw.fm4se;
 
 import edu.mit.csail.sdg.alloy4.*;
 import edu.mit.csail.sdg.ast.Expr;
+import edu.mit.csail.sdg.ast.Func;
 import edu.mit.csail.sdg.ast.Module;
+import edu.mit.csail.sdg.ast.Sig;
 import edu.mit.csail.sdg.parser.CompUtil;
 
 public class Project {
@@ -22,10 +24,27 @@ public class Project {
 
         String fileName = "src/main/resources/dreadburyproject.als";
         Module world = CompUtil.parseEverything_fromFile(rep, null, fileName);
+
+        System.out.println(":: facts");
         SafeList<Pair<String, Expr>> facts = world.getAllFacts();
         for (Pair<String, Expr> f : facts) {
             System.out.println(f.a);
             System.out.println(f.b);
+        }
+
+        System.out.println(":: funcs");
+        SafeList<Func> funcs = world.getAllFunc();
+        for (Func f : funcs) {
+            System.out.println(f.label);
+            System.out.println(f.isPred);
+        }
+
+
+        System.out.println(":: sigs");
+        SafeList<Sig> sigs = world.getAllSigs();
+        for (Sig s : sigs) {
+            System.out.println(s.label);
+            System.out.println(s.builtin);
         }
 
     }
